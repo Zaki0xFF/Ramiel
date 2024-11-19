bd715af14baa95874df654f0b9112c366176a04e
Zaki0xFF <Lelko.patryk@gmail.com>
2024-11-19 22:14:26 +0100

    feat: add test rom

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use instructions::*;
 pub struct CPU {
     registers: Registers,
     pc: u16,
+    sp: u16,
     bus: MemoryBus,
 }
 
@@ -58,14 +59,18 @@ impl CPU {
                 self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
                 self.registers.a = new_value;
             }
-            Instruction::ADDHL() =>{
-                let value = self.registers.get_hl();
-                let hl = self.registers.get_hl();
-                let (new_value, did_overflow) = hl.overflowing_add(value);
-                self.registers.f.zero = new_value == 0;
+            Instruction::ADDHL(target) => {
+                let value = match target {
+                    DoubleTarget::BC => self.registers.get_bc(),
+                    DoubleTarget::DE => self.registers.get_de(),
+                    DoubleTarget::HL => self.registers.get_hl(),
+                    DoubleTarget::SP => self.sp,
+                };
+                let (new_value, did_overflow) = self.registers.get_hl().overflowing_add(value);
                 self.registers.f.subtract = false;
                 self.registers.f.carry = did_overflow;
-                self.registers.f.half_carry = (value & 0x0FFF) + (hl & 0x0FFF) > 0x0FFF;
+                self.registers.f.half_carry = (self.registers.get_hl() & 0xFFF) + (value & 0xFFF) > 0xFFF;
+                self.registers.set_hl(new_value);
             }
             Instruction::ADC(target) => {
                 let value = self.get_register_value(target);
@@ -142,6 +147,14 @@ impl CPU {
                     ArithmeticTarget::L => { self.registers.l += 1;}
                 }
             }
+            Instruction::INCDBL(target) => {
+                match target {
+                    DoubleTarget::BC => { self.registers.set_bc(self.registers.get_bc() + 1); }
+                    DoubleTarget::DE => { self.registers.set_de(self.registers.get_de() + 1); }
+                    DoubleTarget::HL => { self.registers.set_hl(self.registers.get_hl() + 1); }
+                    DoubleTarget::SP => { self.sp += 1; }
+                }
+            }
             Instruction::DEC(target) => {
                 match target {
                     ArithmeticTarget::A => { self.registers.a -= 1;}
@@ -151,6 +164,14 @@ impl CPU {
                     ArithmeticTarget::E => { self.registers.e -= 1;}
                     ArithmeticTarget::H => { self.registers.h -= 1;}
                     ArithmeticTarget::L => { self.registers.l -= 1;}  
+                }
+            }
+            Instruction::DECDBL(target) => {
+                match target {
+                    DoubleTarget::BC => { self.registers.set_bc(self.registers.get_bc() - 1); }
+                    DoubleTarget::DE => { self.registers.set_de(self.registers.get_de() - 1); }
+                    DoubleTarget::HL => { self.registers.set_hl(self.registers.get_hl() - 1); }
+                    DoubleTarget::SP => { self.sp -= 1; }
                 }
             }
             Instruction::CCF() => {
@@ -281,6 +302,7 @@ impl CPU {
             }
             Instruction::NOP() => {}
             Instruction::STOP() => {unimplemented!("STOP instruction not implemented yet")},
+            Instruction::DAA() => {unimplemented!("DAA instruction not implemented yet")},
             _ => { /* TODO: support more instructions */ unimplemented!("not implemented yet cannot execute") }
         }
         return self.pc.wrapping_add(1);
@@ -288,7 +310,33 @@ impl CPU {
 }
 
 fn main() {
-    let test: u8 = 0b1000_1111;
-    let test2: u8 = 0b0000_0000;
-    println!("Hello, world! {:#b}", (test as u16) << 8 | test2 as u16);
+    let mut cpu = CPU {
+        registers: Registers {
+            a: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: FlagsRegister {
+                zero: false,
+                subtract: false,
+                half_carry: false,
+                carry: false,
+            },
+            h: 0,
+            l: 0,
+        },
+        pc: 0,
+        sp: 0,
+        bus: MemoryBus {
+            memory: {
+                let mut mem = [0u8; 0xFFFF];
+                let boot_rom = include_bytes!("../roms/dmg0_boot.bin");
+                mem[..boot_rom.len()].copy_from_slice(boot_rom);
+                mem
+            },
+        },
+    };
+
+    cpu.step();
 }
