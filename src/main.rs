2198a519ef05fb165d0546aef8ac172e395776b8
Zaki0xFF <Lelko.patryk@gmail.com>
2025-03-10 15:14:46 +0100

    refactor: use scope thread

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use log::info;
 use log::LevelFilter;
 use minifb::{Scale, Window, WindowOptions};
 use std::path::Path;
+use std::process::exit;
 use std::sync::mpsc::{self};
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::thread::{self};
 use std::time::Duration;
 
 fn main() {
@@ -20,21 +21,6 @@ fn main() {
     let cpu_state = Arc::new(Mutex::new(CPU::new_with_rom(path, log_sender).unwrap()));
     let render_cpu = Arc::clone(&cpu_state);
     let scale_factor = Scale::X4;
-
-    thread::spawn(move || {
-        while let Ok(log_message) = log_receiver.recv() {
-            info!("{}", log_message);
-        }
-    });
-
-    std::thread::spawn(move || loop {
-        let mut cpu = cpu_state.lock().unwrap();
-        for _ in 0..1000 {
-            cpu.step();
-        }
-        drop(cpu);
-        std::thread::sleep(Duration::from_millis(2));
-    });
 
     let mut window = Window::new(
         "Game Boy Emulator",
@@ -49,11 +35,26 @@ fn main() {
         panic!("{}", e);
     });
 
-    while window.is_open() {
-        let mut state = render_cpu.lock().unwrap();
-        let framebuffer = state.bus.gpu.render_screen();
-        window.update_with_buffer(&framebuffer, 160, 144).unwrap();
-        drop(state);
-        thread::sleep(Duration::from_millis(16));
-    }
+    thread::scope(move |s| {
+        s.spawn(move || loop {
+            let mut cpu = cpu_state.lock().unwrap();
+            cpu.step();
+            drop(cpu);
+            std::thread::sleep(Duration::from_nanos(20));
+        });
+        s.spawn(move || {
+            while let Ok(log_message) = log_receiver.recv() {
+                info!("{}", log_message);
+            }
+            std::thread::sleep(Duration::from_secs(1));
+        });
+        while window.is_open() {
+            let mut state = render_cpu.lock().unwrap();
+            let framebuffer = state.bus.gpu.render_screen();
+            window.update_with_buffer(&framebuffer, 160, 144).unwrap();
+            drop(state);
+            thread::sleep(Duration::from_millis(16));
+        }
+        exit(0);
+    });
 }
